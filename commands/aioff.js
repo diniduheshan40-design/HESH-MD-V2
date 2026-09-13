a7d2c7f15722af014a218bca691455702ae8155c
Nipun_Dhanujaya <moneyheistofc01@gmail.com>
2026-09-13 14:40:36 +0530

    Removed aion and aioff commands to fix error

--- commands/aioff.js
+++
@@ -1,9 +0,0 @@
-const { setAiStatus } = require('../ai');
-module.exports = {
-    name: 'aioff',
-    async execute(sock, msg, args, sender) {
-        await setAiStatus(false);
-        await sock.sendMessage(sender, { text: "*❌ AI System අක්‍රිය කරන ලදී!*" }, { quoted: msg });
-    }
-};
-
--- commands/aion.js
+++
@@ -1,9 +0,0 @@
-const { setAiStatus } = require('../ai');
-module.exports = {
-    name: 'aion',
-    async execute(sock, msg, args, sender) {
-        await setAiStatus(true);
-        await sock.sendMessage(sender, { text: "*✅ AI System සක්‍රිය කරන ලදී!* දැන් .ai මගින් ප්‍රශ්න ඇසිය හැක." }, { quoted: msg });
-    }
-};
-
